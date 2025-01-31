a9766786935c942fa6a8fd83feeb4e78d97354b5
RoldanIsaac <orlandollamos10@gmail.com>
2025-01-31 02:36:47 -0500

    modified:   src/app/app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,4 +10,12 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'login',
+    loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'playlist',
+    loadComponent: () => import('./playlist/playlist.page').then( m => m.PlaylistPage)
+  },
 ];
